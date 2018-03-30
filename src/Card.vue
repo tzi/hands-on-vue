a94726c6b09400fbf6aa3979ec7a1afae6f62264
Thomas Zilliox <thomas@zilliox.me>
2018-03-30 09:44:35 +0200

    Add a Card component

---
+++ src/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="column is-2 has-text-centered">
+        <div class="card">
+            <div class="card-content">
+                <img :src="`src/assets/sprites/${id}.png`"/>
+                <p>
+                    {{ name }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'id',
+            'name'
+        ]
+    }
+</script>
