de19cd1fb027722e9c403f430c6e173291f835ec
Thomas Zilliox <thomas@zilliox.me>
2018-03-30 10:59:20 +0200

    Use the real pokedex API

--- src/pokemonService.js
+++ src/pokemonService.js
@@ -1,16 +1,9 @@
+import axios from 'axios';
+
 export function getPokemonList () {
-    return Promise.resolve([
-        {
-            id: 1,
-            name: 'Bulbasaur'
-        },
-        {
-            id: 2,
-            name: 'Ivysaur'
-        },
-        {
-            id: 3,
-            name: 'Venusaur'
-        }
-    ]);
+    return axios.get('https://pokeapi.co/api/v2/pokedex/1/')
+        .then(result => result.data.pokemon_entries.map(pokemon => ({
+            id: pokemon.entry_number,
+            name: pokemon.pokemon_species.name
+        })));
 }
