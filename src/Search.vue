31a3b3f1dccff4ed9fe0d15e928f511051712cd6
Thomas Zilliox <thomas@zilliox.me>
2018-03-30 10:09:27 +0200

    Introducing model

--- src/Search.vue
+++ src/Search.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="field m-t">
         <div class="control has-icons-left has-icons-right">
-            <input class="input is-danger" type="text" placeholder="Pokemon name" value="">
+            <input class="input is-danger" type="text" placeholder="Pokemon name" value=""
+                v-model="searchValue">
             <span class="icon is-small is-left">
                 <i class="fa fa-search"></i>
             </span>
+            {{ searchValue }}
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                searchValue: ''
+            }
+        }
+    }
+</script>
